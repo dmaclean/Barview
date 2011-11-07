9f787d20c0757c4a9e677c33acf68c6c332175ab
Dan MacLean <dmaclean82@gmail.com>
2011-11-07 13:14:11 -0500

    Added the Terms of Use link to the footer.

--- system/application/views/includes/footer.php
+++ system/application/views/includes/footer.php
@@ -1,25 +1,18 @@
 
 	<!-- Begin footer -->
-<div id="footerWrapper">
-<footer>
-	<!--<div id="poweredby"><a href="http://pyrocms.com/"><img src="/pyrocms/system/cms/themes/default/img/logo.png" alt="Powered by PyroCMS"> Powered by PyroCMS</a></div>-->
-	<ul id="foot-nav" class="unstyled">
-		<li>©2011 Barview.com. All Rights Reserved.</li>
-	</ul>
-</footer>
+<div id="footerWrapper" class="row" style="margin-top:50px;">
+	<div class="span14">©2011 Barview.com. All Rights Reserved.</div>
+	<div class="span2"><a href="<?php echo base_url(); ?>tos.html">Terms of Use</div>
 </div>
 <!-- End footer -->
 
-<!-- Begin scripts -->
-<script type="text/javascript" src="<?php echo base_url(); ?>javascript/localscroll.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>javascript/scrollto.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>javascript/easing.js"></script>
+	<!-- Begin scripts -->
+	<script type="text/javascript" src="<?php echo base_url(); ?>javascript/localscroll.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>javascript/scrollto.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>javascript/easing.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>javascript/cufon.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>javascript/qk.font.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>javascript/global.js"></script>
-<!-- End scripts -->
-	<script type="text/javascript">Cufon.now();</script>
-
+	<script type="text/javascript" src="<?php echo base_url(); ?>javascript/global.js"></script>
 
 	</div>
 	<!-- End pageWrapper -->

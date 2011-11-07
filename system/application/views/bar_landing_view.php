ada13d54297f0e0655b7fb8842c7f9de9eb9f89c
Dan MacLean <dmaclean82@gmail.com>
2011-11-07 12:56:52 -0500

    Changed references of barview.com to bar-view.com and added a paragraph on webcams with an amazon link.

--- system/application/views/bar_landing_view.php
+++ system/application/views/bar_landing_view.php
@@ -1,8 +1,8 @@
 <div class="row">
 	<div class="span8">
-		<h1>Welcome to Barview.com.<h1>
+		<h1>Welcome to Bar-view.com.<h1>
 		<p>
-		Barview.com is a service that allows bars to provide a real-time video 
+		Bar-view.com is a service that allows bars to provide a real-time video 
 		stream of what's happening at their establishment to Barview users.
 		</p>
 	</div>
@@ -10,11 +10,13 @@
 		
 <div class="row">
 	<div class="span8">
-		Barview.com bars
+		<!--Barview.com bars-->
+		Using the bar-view.com broadcasting service requires a webcam to be connected to your computer.  If
+		you don't already have a webcam then you can find several models at <a href="http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=wireless+webcam&x=0&y=0">amazon.com</a>.
 	</div>
 </div>
-<div class="row">
+<!--<div class="row">
 	<div class="span8">
 		Something goes here.
 	</div>
-</div>
+</div>-->

4216b0cbbf35ff09b0f22e221ca41ae0f34ff808
Dan MacLean <dmaclean82@gmail.com>
2011-11-22 23:57:47 -0500

    Added end <a> tag to TOS text.

--- system/application/views/includes/footer.php
+++ system/application/views/includes/footer.php
@@ -2,7 +2,7 @@
 	<!-- Begin footer -->
 <div id="footerWrapper" class="row" style="margin-top:50px;">
 	<div class="span14"><p>©2011 Barview.com. All Rights Reserved.</p></div>
-	<div class="span2"><p><a href="<?php echo base_url(); ?>tos.html">Terms of Use</p></div>
+	<div class="span2"><p><a href="<?php echo base_url(); ?>tos.html">Terms of Use</a></p></div>
 </div>
 <!-- End footer -->
 

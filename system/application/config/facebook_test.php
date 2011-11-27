71b3d141d7fa821d959620766ac4071fc33e2960
Dan MacLean <dmaclean82@gmail.com>
2011-11-27 14:46:58 -0500

    Added config files for test environment

---
+++ system/application/config/facebook_test.php
@@ -0,0 +1,7 @@
+<?php
+
+	$config['facebook_app_id'] 			= '224918730913535';
+	$config['facebook_api_secret'] 		= '5bf00fa4549fe2e6fe48024ad0745ace';
+	$config['facebook_default_scope']	= ''; // E.G 'read_stream,birthday,users_events,rsvp_event'
+	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
+?>
